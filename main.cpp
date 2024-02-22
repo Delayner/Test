dbbe0c7b65018666c857668dcf5de248dd0e775f
Michael Bondar <delayner2018@gmail.com>
2024-02-22 15:56:43 +0200

    Linked list

--- main.cpp
+++ main.cpp
@@ -1,82 +1,187 @@
 #include <iostream>
 
-class Shape {
+
+class LinkedList {
 private:
+    struct Node {
+        int value{};
+        Node *next{};
 
+        Node() = default;
+        Node(int num) : value(num), next(nullptr) {}
+    };
 public:
-    virtual void draw() const = 0;
+    LinkedList() = default;
 
-    virtual double calculateArea() const = 0;
+    void InsertAtBeg(int num) {
+        Node* newNode = new Node(num);
+        newNode->next = head;
 
-    void displayArea() const {
-        std::cout << "Area: " << calculateArea() << std::endl;
+        head = newNode;
     }
 
-    virtual ~Shape() = default;
-};
+    void InsertAtPos(int position, int num) {
+        if(position < 0) {
+            return;
+        }
 
-class Circle : public Shape {
+        if(position == 0) {
+            InsertAtBeg(num);
+            return;
+        }
+
+        Node* newNode = new Node(num);
+
+        Node* current = head;
+        for(int i = 0; i < position - 1 && current != nullptr; ++i) {
+            current = current->next;
+        }
+
+        if(current == nullptr) {
+            return;
+        }
+
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+
+    void InsertAtEnd(int num) {
+        Node* newNode = new Node(num);
+
+        if(head == nullptr) {
+            head = newNode;
+            return;
+        }
+
+        Node* current = head;
+        while(current->next != nullptr) {
+            current = current->next;
+        }
+
+        current->next = newNode;
+    }
+
+    void DeleteAtBeg() {
+        if(head == nullptr) {
+            return;
+        }
+
+        Node* oldHead = head;
+        head = head->next;
+
+        delete oldHead;
+    }
+
+    void DeleteAtPos(int position) {
+        if(head == nullptr) {
+            return;
+        }
+
+        if (position == 0) {
+            DeleteAtBeg();
+            return;
+        }
+
+        Node* pre = head;
+        for(int i = 0; i < position - 1 && pre != nullptr; ++i) {
+            pre = pre->next;
+        }
+
+        if (pre == nullptr) {
+            return;
+        }
+
+        Node* toDelete = pre->next;
+        pre->next = pre->next->next;
+        delete toDelete;
+    }
+
+    void DeleteAtEnd() {
+        if(head == nullptr) {
+            return;
+        }
+
+        if(head->next == nullptr) {
+            delete head;
+            head = nullptr;
+            return;
+        }
+
+        Node* current = head;
+        while(current->next->next != nullptr){
+            current = current->next;
+        }
+
+        delete current->next;
+        current->next = nullptr;
+    }
+
+    void Merge(const LinkedList& other) {
+        if(this->head == nullptr || other.head == nullptr) {
+            return;
+        }
+
+        Node* current= this->head;
+        while(current->next != nullptr)
+        {
+            current = current->next;
+        }
+
+        current->next = other.head;
+    }
+
+    int Search(int element) {
+        int position = 0;
+        Node* current = head;
+
+        while(current != nullptr){
+            if(current->value == element){
+                return position;
+            }
+
+            current = current->next;
+            position++;
+        }
+
+        return -1;
+    }
+
+    void Output() {
+
+        Node* current = head;
+        while(current != nullptr){
+            std::cout << current->value << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
 private:
-    double radius;
-public:
-    Circle(double r) : radius(r) {}
-
-    void draw() const override {
-        std::cout << "Drawing a circle" << std::endl;
-    }
-
-    double calculateArea() const override {
-        return 3.14 * radius * radius;
-    }
-};
-
-class Rectangle : public Shape {
-private:
-    double side_1, side_2;
-public:
-    Rectangle(double a, double b) : side_1(a), side_2(b) {}
-
-    void draw() const override {
-        std::cout << "Drawing a Rectangle" << std::endl;
-    }
-
-    double calculateArea() const override {
-        return side_1 * side_2;
-    }
-};
-
-class Triangle : public Shape {
-private:
-    double side, height;
-public:
-    Triangle(double a, double h) : side(a), height(h) {}
-
-    void draw() const override {
-        std::cout << "Drawing a Triangle" << std::endl;
-    }
-
-    double calculateArea() const override {
-        return 0.5 * side * height;
-    }
+    Node* head{};
 };
 
 int main() {
-    Circle circle(5.0);
-    Rectangle rectangle(4, 5);
-    Triangle triangle(3, 4);
+    LinkedList list1;
+    LinkedList list2;
 
-    Shape *shape1 = &circle;
-    Shape *shape2 = &rectangle;
-    Shape *shape3 = &triangle;
+    list1.InsertAtEnd(2);
+    list1.InsertAtEnd(3);
+    list1.InsertAtEnd(4);
 
-    shape1->draw();
-    shape2->draw();
-    shape3->draw();
+    list2.InsertAtEnd(5);
+    list2.InsertAtEnd(6);
+    list2.InsertAtEnd(7);
 
-    shape1->displayArea();
-    shape2->displayArea();
-    shape3->displayArea();
+//    list1[2] = 999;
 
-    return 0;
+    list1.Merge(list2);
+
+    list1.Output();
 }
+
+
+
+
+
+
+
 
